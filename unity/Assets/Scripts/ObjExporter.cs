a7934ea6c817acb82ef00656da6262080ae7447d
wpumacay <wpumacay@gmail.com>
2025-04-09 18:15:58 -0500

    WIP exporting materials

--- unity/Assets/Scripts/ObjExporter.cs
+++ unity/Assets/Scripts/ObjExporter.cs
@@ -1,20 +1,33 @@
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using UnityEditor;
 using UnityEngine;
 
+public class ObjAssetTexture
+{
+    public string name = string.Empty;
+    public string path = string.Empty;
+    public Texture texture = null;
+}
+
 public class ObjExporter
 {
     private int m_StartIndex = 0;
+    private string m_ExportDirectory = string.Empty;
+    private Dictionary<string, ObjAssetTexture> m_Assets;
 
-    public void Start()
+    public void Start(string exportDirectory)
     {
         m_StartIndex = 0;
+        m_ExportDirectory = exportDirectory;
+        m_Assets = new Dictionary<string, ObjAssetTexture>();
     }
 
     public void End()
     {
         m_StartIndex = 0;
+        m_Assets.Clear();
     }
 
     public string MeshToString(MeshFilter mf, Transform tf)
@@ -22,6 +35,9 @@ public class ObjExporter
         Vector3 scale = tf.localScale;
         Vector3 pos = tf.localPosition;
         Quaternion rot = tf.localRotation;
+        GameObject go = tf.gameObject;
+
+        var meshRenderer = go.GetComponent<MeshRenderer>();
 
         int numVertices = 0;
         Mesh mesh = mf.sharedMesh;
@@ -50,6 +66,15 @@ public class ObjExporter
         }
         for (var idx = 0; idx < mesh.subMeshCount; idx++)
         {
+            // TODO(wilbert): Handle case with multiple materials. Currently we
+            // are assuming that all objects have only one material at most
+
+            if (meshRenderer != null && idx < meshRenderer.sharedMaterials.Length)
+            {
+                var matName = meshRenderer.sharedMaterials[idx].name;
+                strBuilder.AppendLine($"usemtl {matName}");
+            }
+
             int[] triangles = mesh.GetTriangles(idx);
             for (var i = 0; i < triangles.Length; i += 3)
             {
@@ -68,43 +93,108 @@ public class ObjExporter
         return strBuilder.ToString();
     }
 
-    public string ProcessTransform(Transform tf, bool makeSubmeshes)
+    public string MaterialToString(Material mat)
     {
         var strBuilder = new StringBuilder();
 
-        if (tf.TryGetComponent<MeshFilter>(out var meshFilter))
+        strBuilder.AppendLine($"newmtl {mat.name}");
+        strBuilder.AppendLine($"Ka {mat.color.r} {mat.color.g} {mat.color.b}");
+        strBuilder.AppendLine($"Kd {mat.color.r} {mat.color.g} {mat.color.b}");
+        if (mat.color.a < 1.0f)
         {
-            strBuilder.AppendLine($"# {tf.name}");
-            strBuilder.AppendLine($"#---------------");
-
-            if (makeSubmeshes)
+            strBuilder.AppendLine($"Tr {1 - mat.color.a}");
+            strBuilder.AppendLine($"d {mat.color.a}");
+        }
+        if (mat.HasProperty("_SpecColor"))
+        {
+            var specColor = mat.GetColor("_SpecColor");
+            strBuilder.AppendLine($"Ks {specColor.r} {specColor.g} {specColor.b}");
+        }
+        if (mat.HasProperty("_MainTex"))
+        {
+            var texture = mat.GetTexture("_MainTex") as Texture2D;
+            if (texture != null)
             {
-                strBuilder.AppendLine($"g {tf.name}");
+                strBuilder.AppendLine($"map_Kd textures/{texture.name}.png");
+                var asset = new ObjAssetTexture
+                {
+                    name = texture.name,
+                    path = Path.Combine(m_ExportDirectory, "textures", texture.name + ".png"),
+                    texture = texture
+                };
+                m_Assets.Add(asset.name, asset);
             }
-
-            strBuilder.Append(MeshToString(meshFilter, tf));
         }
 
-        for (var i = 0; i < tf.childCount; i++)
-        {
-            strBuilder.Append(ProcessTransform(tf.GetChild(i), makeSubmeshes));
-        }
+        strBuilder.AppendLine("illum 2");
 
         return strBuilder.ToString();
     }
 
-    public string Export(GameObject go, string fileName, bool makeSubmeshes)
+    public (string, string) ProcessTransform(Transform tf, bool makeSubmeshes)
     {
-        Start();
+        var strMesh = new StringBuilder();
+        var strMaterial = new StringBuilder();
 
-        var strBuilder = new StringBuilder();
+        if (tf.TryGetComponent<MeshFilter>(out var meshFilter))
+        {
+            strMesh.AppendLine($"# {tf.name}");
+            strMesh.AppendLine($"#---------------");
 
-        strBuilder.AppendLine("----------------------------------------------");
-        strBuilder.AppendLine($"# {fileName}.obj");
-        strBuilder.AppendLine($"# Exported using iThor OBJ exporter");
-        strBuilder.AppendLine($"# {System.DateTime.Now.ToLongDateString()}");
-        strBuilder.AppendLine($"# {System.DateTime.Now.ToLongTimeString()}");
-        strBuilder.AppendLine("----------------------------------------------");
+            if (makeSubmeshes)
+            {
+                strMesh.AppendLine($"g {tf.name}");
+            }
+
+            strMesh.Append(MeshToString(meshFilter, tf));
+        }
+
+        if (tf.TryGetComponent<MeshRenderer>(out var meshRenderer))
+        {
+            strMaterial.AppendLine($"# {tf.name}");
+            strMaterial.AppendLine($"#---------------");
+
+            foreach (var mat in meshRenderer.sharedMaterials)
+            {
+                strMaterial.Append(MaterialToString(mat));
+            }
+        }
+
+        for (var i = 0; i < tf.childCount; i++)
+        {
+            var (substrMesh, substrMaterial) = ProcessTransform(tf.GetChild(i), makeSubmeshes);
+            strMesh.Append(substrMesh);
+            strMaterial.Append(substrMaterial);
+        }
+
+        return (strMesh.ToString(), strMaterial.ToString());
+    }
+
+    public (string, string) Export(
+        GameObject go,
+        string fileName,
+        string fileDirectory,
+        bool makeSubmeshes
+    )
+    {
+        Start(fileDirectory);
+
+        var strMesh = new StringBuilder();
+        var strMaterial = new StringBuilder();
+
+        strMesh.AppendLine("----------------------------------------------");
+        strMesh.AppendLine($"# {fileName}.obj");
+        strMesh.AppendLine($"# Exported using iThor OBJ exporter");
+        strMesh.AppendLine($"# {System.DateTime.Now.ToLongDateString()}");
+        strMesh.AppendLine($"# {System.DateTime.Now.ToLongTimeString()}");
+        strMesh.AppendLine("----------------------------------------------");
+
+        strMaterial.AppendLine("---------------------------------------------");
+        strMaterial.AppendLine($"# {fileName}.mtl");
+        strMaterial.AppendLine($"# Exported using iThor OBJ exporter");
+        strMaterial.AppendLine($"# {System.DateTime.Now.ToLongDateString()}");
+        strMaterial.AppendLine($"# {System.DateTime.Now.ToLongTimeString()}");
+        strMaterial.AppendLine("---------------------------------------------");
 
         var tf = go.transform;
         var originalPosition = tf.position;
@@ -112,16 +202,18 @@ public class ObjExporter
 
         if (!makeSubmeshes)
         {
-            strBuilder.AppendLine($"g {tf.name}");
+            strMesh.AppendLine($"g {tf.name}");
         }
 
-        strBuilder.Append(ProcessTransform(tf, makeSubmeshes));
+        var (substrMesh, substrMaterial) = ProcessTransform(tf, makeSubmeshes);
+        strMesh.Append(substrMesh);
+        strMaterial.Append(substrMaterial);
 
         tf.position = originalPosition;
 
         End();
 
-        return strBuilder.ToString();
+        return (strMesh.ToString(), strMaterial.ToString());
     }
 }
 
@@ -148,13 +240,23 @@ public class ObjExporterTools : MonoBehaviour
         }
 
         string meshName = Selection.activeGameObject.name;
-        string fileName = EditorUtility.SaveFilePanel("Export .obj file", "", meshName, "obj");
+        string fileNameObj = EditorUtility.SaveFilePanel("Export .obj file", "", meshName, "obj");
+        string fileNameMtl = Path.ChangeExtension(fileNameObj, "mtl");
+        string fileNameNoExt = Path.GetFileNameWithoutExtension(fileNameObj);
+        string fileDirectory = Path.GetDirectoryName(fileNameObj);
 
         var objExporter = new ObjExporter();
-        var objStr = objExporter.Export(Selection.activeGameObject, fileName, makeSubmeshes);
+        var (objMeshStr, objMaterialStr) = objExporter.Export(
+            Selection.activeGameObject,
+            fileNameNoExt,
+            fileDirectory,
+            makeSubmeshes
+        );
 
-        WriteToFile(fileName, objStr);
-        Debug.Log($"Exported Mesh: {fileName}");
+        WriteToFile(fileNameObj, objMeshStr);
+        WriteToFile(fileNameMtl, objMaterialStr);
+        Debug.Log($"Exported Mesh: {fileNameObj}");
+        Debug.Log($"Exported Material: {fileNameMtl}");
     }
 
     static void WriteToFile(string fileName, string content)
